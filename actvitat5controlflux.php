b5ed15dba8e7a9568ea44293282fcbd2b0ce6ae0
mohamedmendou2 <mmendou@iesjulioantonio.cat>
2025-10-30 10:47:59 +0100

    Update actvitat5controlflux.php

--- actvitat5controlflux.php
+++ actvitat5controlflux.php
@@ -17,23 +17,21 @@
 
     <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Obtenir la data de naixement introduïda pel formulari
+        
         $data_naixement = $_POST['data_naixement'];
 
-        // Convertir la data de naixement a un objecte DateTime
+ 
         $data_naixement = new DateTime($data_naixement);
         
-        // Obtenir la data actual
         $data_actual = new DateTime();
 
-        // Calcular la diferència en anys entre les dues dates
+        
         $diferencia = $data_actual->diff($data_naixement);
         $edat = $diferencia->y;
 
-        // Mostrar l'edat
+     
         echo "<p>Tens $edat anys.</p>";
 
-        // Comprovar si és major d'edat (18 anys o més)
         if ($edat >= 18) {
             echo "<p>Ets major d'edat.</p>";
         } else {
