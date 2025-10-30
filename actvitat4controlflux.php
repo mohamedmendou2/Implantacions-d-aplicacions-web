1256c40dc502a85fa60774eb2d580d967924d875
mohamedmendou2 <mmendou@iesjulioantonio.cat>
2025-10-30 10:27:26 +0100

    Update actvitat4controlflux.php

--- actvitat4controlflux.php
+++ actvitat4controlflux.php
@@ -17,12 +17,12 @@
 
     <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Obtenir la nota introduïda pel formulari
+        
         $nota = $_POST['nota'];
 
-        // Comprovar si la nota és dins de l'interval 1-10
+    
         if ($nota >= 1 && $nota <= 10) {
-            // Comprovar si la nota és aprovada o suspesa
+          
             if ($nota >= 5) {
                 echo "<p>Aprovat!</p>";
             } else {
